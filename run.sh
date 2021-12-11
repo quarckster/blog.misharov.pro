443f06bc602d01de68fed605b3706c7be42cbd5f
Dmitry Misharov <mail@misharov.pro>
2021-12-11 17:39:15 +0100

    Updated run script

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ podman run -it -e BUNDLE_BIN=/mnt/vendor/bundle/bin \
                -e BUNDLE_PATH=/mnt/vendor \
                -w /mnt \
                -v .:/mnt \
-               -p 4000:4000 docker.io/library/ruby:2.7 bash
+               -p 4000:4000 docker.io/library/ruby:2.7 \
+               bundle exec jekyll serve -H 0.0.0.0
+
